5ecbda0b7f1d39dd1bbd3e965bc739926b29a07b
Jerry Ling <proton@jling.dev>
2021-08-05 11:57:18 +0200

    Update hist1d.jl

--- src/hist1d.jl
+++ src/hist1d.jl
@@ -14,7 +14,7 @@ Base.unlock(h::Hist1D) = unlock(h.hlock)
     sample(h::Hist1D, n::Int=1)
 
 Sample a histogram's with weights equal to bin count, `n` times.
-The sampled values are the bin centers.
+The sampled values are the bins' lower edges.
 """
 function sample(h::Hist1D; n::Int=1)
     StatsBase.sample(binedges(h)[1:end-1], Weights(bincounts(h)), n)
