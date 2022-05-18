ccca80751056fb2f9f8b4fc5b5adc36fcf25db54
Moelf <proton@jling.dev>
2022-05-18 16:10:57 -0400

    add Hist2D recipe

--- src/plot.jl
+++ src/plot.jl
@@ -4,3 +4,12 @@
     y:=h.hist.weights
     ()
 end
+
+@recipe function f(h::Hist2D)
+    seriestype --> :bins2d
+    x := h.hist.edges[1]
+    y := h.hist.edges[2]
+    z := (surf = h.hist.weights, )
+    ()
+end
+
