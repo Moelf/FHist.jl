3a44ff908d2e1aa8d0ce8e4c084149970746a35d
Jerry Ling <proton@jling.dev>
2022-06-09 01:18:13 -0400

    add legends example for stackedhist

--- src/makie.jl
+++ src/makie.jl
@@ -1,13 +1,31 @@
 using .Makie
 
 """
-    stackedhist(hs:AbstractVector{<:Hist1D}; errors=true)
+    stackedhist(hs:AbstractVector{<:Hist1D}; errors=true, color=Makie.wong_colors())
 
 Plot a vector of 1D histograms stacked, use `errors` to show or hide error bar in the plot.
+
+`color` should be a vector of colors that is at least `length(hs)` long. See below example
+regarding how to make legends semi-manually.
+
+# Examples
+h1 = Hist1D(randn(1000), -3:0.3:3)
+h2 = Hist1D(randn(10000), -3:0.3:3)
+
+fig, a, p = stackedhist([h1, h2, h2])
+labels = ["ZZ", "Z+jets", "ttbarZ"]
+elements = [PolyElement(polycolor = p.attributes.color[][i]) for i in 1:length(labels)]
+title = "Processes"
+
+Legend(fig[1,2], elements, labels, title)
+fig
 """
 @recipe(StackedHist) do scene
     Attributes(
-        errors = true
+        errors = true,
+        color = Makie.wong_colors(),
+        labels = nothing,
+        whiskerwidth = 15
     )
 end
 
@@ -26,14 +44,16 @@ function Makie.plot!(input::StackedHist{<:Tuple{AbstractVector{<:Hist1D}}})
     totals = Measurements.value.(mes)
     errs = Measurements.uncertainty.(mes)
     
+    c = input[:color][]
+    length(c) < Nhist && throw("provided $(length(c)) colors, not enough for $Nhist histograms")
     Makie.barplot!(input, xs, ys;
         stack = grp,
-        color = grp,
-        gap = 0
+        color = c[grp],
+        gap = 0,
     )
     
-    if input.errors[]
-        errorbars!(input, centers, totals, errs/2, whiskerwidth = 15)
+    if input[:errors][]
+        errorbars!(input, centers, totals, errs/2, whiskerwidth = input[:whiskerwidth][])
     end
     input
 end
@@ -43,7 +63,8 @@ Makie.convert_arguments(P::Type{<:Stairs}, h::Hist1D) = convert_arguments(P, bin
 Makie.convert_arguments(P::Type{<:BarPlot}, h::Hist1D) = convert_arguments(P, bincenters(h), bincounts(h))
 function Makie.plot!(input::Hist{<:Tuple{<:Hist1D}})
     h = input[1][]
-    Makie.barplot!(input, h; gap = 0)
+    L = haskey(input, :label) ? input[:label][] : nothing
+    Makie.barplot!(input, h; gap = 0, label = L)
     input
 end
 
