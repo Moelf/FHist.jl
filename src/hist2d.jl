1d5036cd79f23de3ec9ea169fd6d1c38ada3bac0
Johnson, Bo <johnsonba@ornl.gov>
2023-10-27 10:53:31 -0400

    restrict Hist2D

--- src/hist2d.jl
+++ src/hist2d.jl
@@ -342,3 +342,38 @@ function project(h::Hist2D, axis::Symbol, val::Real)
     end
     h
 end
+
+function restrict(h::Hist2D, xlow=-Inf, xhigh=Inf, ylow=-Inf, yhigh=Inf)
+    xsel = xlow .<= bincenters(h)[1] .<= xhigh
+    ysel = ylow .<= bincenters(h)[2] .<= yhigh
+    @assert sum(xsel) > 0 "No bin centers contained in [$(xlow), $(xhigh)]"
+    @assert sum(ysel) > 0 "No bin centers contained in [$(ylow), $(yhigh)]"
+    xedgesel = push!(copy(xsel), false)
+    yedgesel = push!(copy(ysel), false)
+
+    xlastidx = findlast(xedgesel)
+    if xlastidx != nothing
+        xedgesel[xlastidx+1] = 1
+    end
+
+    ylastidx = findlast(yedgesel)
+    if ylastidx != nothing
+        yedgesel[ylastidx+1] = 1
+    end
+
+    xc = bincounts(h)[1][xsel]
+    xedges = binedges(h)[1][xedgesel]
+    if _is_uniform_bins(xedges)
+        xedges = range(first(xedges), last(xedges), length=length(xedges))
+    end
+
+    yc = bincounts(h)[2][ysel]
+    yedges = binedges(h)[2][yedgesel]
+    if _is_uniform_bins(yedges)
+        yedges = range(first(yedges), last(yedges), length=length(yedges))
+    end
+
+    sumw2 = h.sumw2[xsel,ysel]
+
+    @show xc yc sumw2
+end
