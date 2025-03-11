fbc80a762bbcd6a3934bbe29d0869b171d54725d
Beforerr <58776897+Beforerr@users.noreply.github.com>
2025-03-11 16:46:44 -0700

    fix: makie0.22 Change deprecation warnings to errors (#140)

--- ext/FHistMakieExt.jl
+++ ext/FHistMakieExt.jl
@@ -258,11 +258,13 @@ function Makie.convert_arguments(P::Type{<:Heatmap}, h2d::Hist2D)
     convert_arguments(P, binedges(h2d)..., replace(counts, z => NaN))
 end
 
+_to_endpoints(binedge) = (first(binedge), last(binedge))
+
 Makie.MakieCore.plottype(::Hist3D) = Volume
 function Makie.convert_arguments(P::Type{<:Volume}, h::Hist3D)
     counts = bincounts(h)
     z = zero(eltype(counts))
-    convert_arguments(P, binedges(h)..., replace(counts, z => NaN))
+    convert_arguments(P, _to_endpoints.(binedges(h))..., replace(counts, z => NaN))
 end
 
 """
