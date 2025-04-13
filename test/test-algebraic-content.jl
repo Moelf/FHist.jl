d529ad3056fc4c398b17bd2979da5d1ae3c062c1
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2025-04-13 16:41:31 +0200

    update the test

--- test/test-algebraic-content.jl
+++ test/test-algebraic-content.jl
@@ -1,9 +1,8 @@
 using Test
 using FHist
 using FHist.Statistics
-
 @testset "Negative content" begin
-    h0 = Hist1D([1, 3, 4, 5, 6, 7, 3]; binedges = range(0, 1, 3))
+    h0 = Hist1D([1, 3, 4, 5, 6, 7, 3]; binedges = range(0, 10, 5))
     original_mean = mean(h0)
     h0.bincounts .-= mean(h0.bincounts)  # This makes sum of weights ≈ 0
     @test abs(sum(h0.bincounts)) < 1e-10  # Verify weights sum to approximately zero
