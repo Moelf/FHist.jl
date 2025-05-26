8a6f8aa163ec3f677a6c653246935382e51e2724
Jerry Ling <proton@jling.dev>
2025-05-25 22:08:20 -0400

    try benchmark (#145)
    * try benchmark
    
    * don't need to run it

--- benchmark/main.jl
+++ benchmark/main.jl
@@ -20,23 +20,19 @@ function hist1d_loop_atomic(xs; binedges)
     hist
 end
 
-suite = BenchmarkGroup()
+const SUITE = BenchmarkGroup()
 
-suite["Uniform Binning"] = BenchmarkGroup()
-suite["Non-uniform Binning"] = BenchmarkGroup()
+SUITE["Uniform Binning"] = BenchmarkGroup()
+SUITE["Non-uniform Binning"] = BenchmarkGroup()
 
-suite["Uniform Binning"]["One-shot Uniform input"] = @benchmarkable Hist1D(uniform_xs; binedges = 0:0.1:1)
-suite["Uniform Binning"]["One-shot Gaussian input"] = @benchmarkable Hist1D(gauss_xs; binedges = 0:0.1:1)
-suite["Uniform Binning"]["One-shot Uniform input with weights"] = @benchmarkable Hist1D(uniform_xs; weights, binedges = 0:0.1:1)
-suite["Uniform Binning"]["One-shot Gaussian input with weights"] = @benchmarkable Hist1D(gauss_xs; weights, binedges = 0:0.1:1)
-suite["Uniform Binning"]["push!-loop Uniform input"] = @benchmarkable hist1d_loop(uniform_xs; binedges = 0:0.1:1)
-suite["Uniform Binning"]["atomic_push!-loop Gaussian input"] = @benchmarkable hist1d_loop_atomic(gauss_xs; binedges = 0:0.1:1)
+SUITE["Uniform Binning"]["One-shot Uniform input"] = @benchmarkable Hist1D(uniform_xs; binedges = 0:0.1:1)
+SUITE["Uniform Binning"]["One-shot Gaussian input"] = @benchmarkable Hist1D(gauss_xs; binedges = 0:0.1:1)
+SUITE["Uniform Binning"]["One-shot Uniform input with weights"] = @benchmarkable Hist1D(uniform_xs; weights, binedges = 0:0.1:1)
+SUITE["Uniform Binning"]["One-shot Gaussian input with weights"] = @benchmarkable Hist1D(gauss_xs; weights, binedges = 0:0.1:1)
+SUITE["Uniform Binning"]["push!-loop Uniform input"] = @benchmarkable hist1d_loop(uniform_xs; binedges = 0:0.1:1)
+SUITE["Uniform Binning"]["atomic_push!-loop Gaussian input"] = @benchmarkable hist1d_loop_atomic(gauss_xs; binedges = 0:0.1:1)
 
-suite["Non-Uniform Binning"]["One-shot Uniform input"] = @benchmarkable Hist1D(uniform_xs; binedges = [0, 0.1, 0.3, 0.5, 1.0])
-suite["Non-Uniform Binning"]["One-shot Gaussian input"] = @benchmarkable Hist1D(gauss_xs; binedges = [0, 0.1, 0.3, 0.5, 1.0])
-suite["Non-Uniform Binning"]["One-shot Uniform input with weights"] = @benchmarkable Hist1D(uniform_xs; weights, binedges = [0, 0.1, 0.3, 0.5, 1.0])
-suite["Non-Uniform Binning"]["One-shot Gaussian input with weights"] = @benchmarkable Hist1D(gauss_xs; weights, binedges = [0, 0.1, 0.3, 0.5, 1.0])
-
-tune!(suite)
-
-run(suite; verbose=true, seconds = 2)
+SUITE["Non-Uniform Binning"]["One-shot Uniform input"] = @benchmarkable Hist1D(uniform_xs; binedges = [0, 0.1, 0.3, 0.5, 1.0])
+SUITE["Non-Uniform Binning"]["One-shot Gaussian input"] = @benchmarkable Hist1D(gauss_xs; binedges = [0, 0.1, 0.3, 0.5, 1.0])
+SUITE["Non-Uniform Binning"]["One-shot Uniform input with weights"] = @benchmarkable Hist1D(uniform_xs; weights, binedges = [0, 0.1, 0.3, 0.5, 1.0])
+SUITE["Non-Uniform Binning"]["One-shot Gaussian input with weights"] = @benchmarkable Hist1D(gauss_xs; weights, binedges = [0, 0.1, 0.3, 0.5, 1.0])
