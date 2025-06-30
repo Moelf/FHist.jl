0e0bdae7fbd8a6f3cffa073bdd37ef2b65a43e30
Moelf <proton@jling.dev>
2025-06-30 12:41:34 -0400

    trait based

--- ext/FHistMakieExt.jl
+++ ext/FHistMakieExt.jl
@@ -273,10 +273,10 @@ function Makie.convert_arguments(P::Type{<:Heatmap}, h2d::Hist2D)
     convert_arguments(P, binedges(h2d)..., replace(counts, z => NaN))
 end
 
-function Makie.convert_arguments(P::Type{<:Contour}, h2d::Hist2D)
+function Makie.convert_arguments(::VertexGrid, h2d::Hist2D)
     counts = bincounts(h2d)
     z = zero(eltype(counts))
-    convert_arguments(P, bincenters(h2d)..., replace(counts, z => NaN))
+    (bincenters(h2d)..., replace(counts, z => NaN))
 end
 
 _to_endpoints(binedge) = (first(binedge), last(binedge))
