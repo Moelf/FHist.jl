fix units

C_FHist/test.py
@@ -1,6 +1,7 @@
 import ctypes
 import numpy as np
 import timeit
+import time
 
 lib = ctypes.CDLL('./libfhistjl.so')
 lib.hist1d.argtypes = [
@@ -36,5 +37,5 @@ for N_input in [10**3, 10**4, 10**5, 10**6]:
                                                                                                                        1.0))))
     np_timer = timeit.Timer(lambda: np.histogram(input_data, bins=bins, range=(0.0, 1.0)))
     jl_timer = timeit.Timer(lambda: jlhist(input_data, bins=bins, range=(0.0, 1.0)))
-    print(f"Numpy    time (μs): {np.min(np_timer.repeat(number=5, repeat=200)) / 5 * 1000}")
-    print(f"FHist.jl time (μs): {np.min(jl_timer.repeat(number=5, repeat=200)) / 5 * 1000}")
+    print(f"Numpy    time (ms): {np.min(np_timer.repeat(number=5, repeat=200)) / 5 * 1000}")
+    print(f"FHist.jl time (ms): {np.min(jl_timer.repeat(number=5, repeat=200)) / 5 * 1000}")