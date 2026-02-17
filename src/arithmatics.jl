d57f8a75804a055b112b11ed48096a9c5f5c3534
abhro <5664668+abhro@users.noreply.github.com>
2026-02-17 13:48:46 -0500

    Update syntax highlight tags in example blocks

--- src/arithmatics.jl
+++ src/arithmatics.jl
@@ -68,9 +68,10 @@ accurate algorithm than the naive `S / sqrt(B)`
 Ref: <https://cds.cern.ch/record/2736148/files/ATL-PHYS-PUB-2020-025.pdf>
 
 ## Example:
-```julia
-h1 = Hist1D(rand(1000);  binedges = [0, 0.5])
-h2 = Hist1D(rand(10000); binedges = [0, 0.5]);
+```julia-repl
+julia> h1 = Hist1D(rand(1000);  binedges = [0, 0.5]);
+
+julia> h2 = Hist1D(rand(10000); binedges = [0, 0.5]);
 
 julia> s1 = significance(h1,h2)
 (6.738690967342175, 0.3042424717261312)
